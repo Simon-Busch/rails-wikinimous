2664935d9a213a79dc6553ef9f0bea8d0d5a8bca
Simon Busch <simon.busch@live.fr>
2020-11-17 13:59:25 +0100

    done with index, create, edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'articles', to: 'article#index'
+
+  get 'articles/new', to: 'article#new'
+  post 'articles', to:'article#create'
+
+  get 'article/:id',  to: 'article#show', as: :article
+
+  get 'article/:id/edit',  to: 'article#edit', as: :edit_article
+  patch 'article/:id', to: 'article#update'
 end
